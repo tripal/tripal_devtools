a2f34701763bf1a5ac11eadb6bf4db407669cae8
Lacey-Anne Sanderson <laceyannesanderson@gmail.com>
2025-12-09 18:00:32 +0000

    Fix field prompt and default scenario record_id.

--- src/Drush/Generators/TestChadoFieldTypeGenerator.php
+++ src/Drush/Generators/TestChadoFieldTypeGenerator.php
@@ -81,11 +81,13 @@ class TestChadoFieldTypeGenerator extends BaseGenerator {
 
     // For each field attached to this bundle, ask if they want to add it to
     // the system under test.
+    $dont_prompt_fields = $this->prompt->confirm('Do you want to exclude some fields for this content type', FALSE);
     foreach ($this->field_definitions as $field_name => $field_defn) {
       if (get_class($field_defn) == 'Drupal\field\Entity\FieldConfig') {
-        //if ($this->prompt->confirm(" - Add $field_name to the system-under-test?")) {
-        $this->addFieldInfo($vars['bundle_id'], $field_defn, $test_info_yaml);
-        //}
+        if (($dont_prompt_fields === FALSE) or $this->prompt->confirm("\tAdd $field_name to the system-under-test?")) {
+          print "\tAdding $field_name to the system-under-test.\n";
+          $this->addFieldInfo($vars['bundle_id'], $field_defn, $test_info_yaml);
+        }
       }
     }
 
@@ -199,21 +201,47 @@ class TestChadoFieldTypeGenerator extends BaseGenerator {
     ];
 
     foreach (['create', 'edit'] as $first_lvl) {
-      $scenario[$first_lvl] = [];
-      foreach (['user_input', 'expected'] as $second_lvl) {
-        $scenario[$first_lvl][$second_lvl] = [];
-        foreach ($test_info_yaml['system-under-test']['fields'] as $field_yml) {
-          $field_name = $field_yml['name'];
-          // @todo add these values based on the field definition.
-          $scenario[$first_lvl][$second_lvl][$field_name] = [
-            'record_id' => 0,
-            'value' => '',
-          ];
+      $scenario[$first_lvl] = [
+        'user_input' => [],
+        'expected' => [],
+      ];
+      foreach ($test_info_yaml['system-under-test']['fields'] as $field_yml) {
+        $field_name = $field_yml['name'];
+        $field_class = $field_yml['type_class'];
+        // Get each field to generate some sample values for testing.
+        $sample_value = $field_class::generateSampleValue($this->field_definitions[$field_name]);
+        if ($sample_value === NULL) {
+          $sample_value = [];
+        }
+        // Now set the sample value for both our input and expected.
+        $scenario[$first_lvl]['user_input'][$field_name] = $sample_value;
+        $scenario[$first_lvl]['expected'][$field_name] = $sample_value;
+        // We also need to update the record_id to match expectations.
+        if ($first_lvl == 'create') {
+          $user_input_record_id = 0;
+          $expected_record_id = 1;
+        }
+        else {
+          $user_input_record_id = 1;
+          $expected_record_id = 1;
+        }
+        // If single value field then set record_id directly.
+        if (array_key_exists('record_id', $scenario[$first_lvl]['user_input'][$field_name])) {
+          $scenario[$first_lvl]['user_input'][$field_name]['record_id'] = $user_input_record_id;
+          $scenario[$first_lvl]['expected'][$field_name]['record_id'] = $expected_record_id;
+        }
+        // If multi-value field then set record_id for each item.
+        elseif (array_key_exists(0, $scenario[$first_lvl]['user_input'][$field_name]) and array_key_exists('record_id', $scenario[$first_lvl]['user_input'][$field_name][0])) {
+          foreach (array_keys($scenario[$first_lvl]['user_input'][$field_name]) as $delta) {
+            $scenario[$first_lvl]['user_input'][$field_name][$delta]['record_id'] = $user_input_record_id;
+            $scenario[$first_lvl]['expected'][$field_name][$delta]['record_id'] = $expected_record_id;
+          }
         }
       }
     }
 
     $test_info_yaml['scenarios'][] = $scenario;
+
   }
 
 }
